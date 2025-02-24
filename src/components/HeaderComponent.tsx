54269d9427aa4e03d70b6181175feb7ef246aaac
paho <pikopavel@gmail.com>
2025-02-24 19:35:55 +0300

    header style test

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import cart from '../images/cart.png';
+import logo from '../images/logo.png';
+import styles from './HeaderComponentStyles.module.css';
 
 export const HeaderComponent = () => {
     return (
-        <div className="headerContainer">
-            <div className="">
-                <Link to={"/"} className="button">Home</Link>
-                <Link to={"/ShoppingCartPage"} className="button">
-                    <img src={cart} alt="Cart" />
+        <div className={styles.headerContainer}>
+            <div className={styles.link}>
+                <Link to={"/"} className={styles.button}>
+                <img className={styles.home} src={logo} alt="logo" /> <h1 className={styles.logotext}>mxk$hop</h1>
+                </Link>
+                <Link to={"/ShoppingCartPage"} className={styles.button}>
+                    <img className={styles.cart} src={cart} alt="Cart" />
                 </Link>
             </div>
         </div>
