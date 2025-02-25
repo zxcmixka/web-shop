9ec6a6f819dd7f184a022432f16353e4fa74b27a
paho <pikopavel@gmail.com>
2025-02-25 22:07:29 +0300

    css add files

--- src/page/MainPage/MainPage.tsx
+++ src/page/MainPage/MainPage.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import styles from './MainPage.module.css'
 
 export const MainPage = () => {
   return (
     <div>
         <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        <h1>52</h1>
+        
     </div>
   );
 }
--- src/page/ShoppingCartPage/ShoppingCartPage.tsx
+++ src/page/ShoppingCartPage/ShoppingCartPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './ShoppingCartPage.module.css'
 
 export const ShoppingCartPage = () => {
   return (
