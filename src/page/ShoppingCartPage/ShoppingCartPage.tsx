c3dbcc37ea26a572fd1845b983d5135278d04802
paho <pikopavel@gmail.com>
2025-03-04 20:01:34 +0300

    TEST SERVER WORK

--- src/page/ShoppingCartPage/ShoppingCartPage.tsx
+++ src/page/ShoppingCartPage/ShoppingCartPage.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import styles from './ShoppingCartPage.module.css'
+import axios from "axios";
+
+export const loginUser = async () => {
+  try {
+      const response = await axios.post("http://localhost:5000/login", {
+          email: "test@example.com",
+          password: "password123",
+      });
+      console.log("Токен:", response.data.token);
+  } catch (error) {
+      console.error("Ошибка входа:",);
+  }
+};
+loginUser();
+
 
 export const ShoppingCartPage = () => {
   return (
